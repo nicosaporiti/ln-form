9b9be73393e7cb0b3a00f46873b00848741ae1a7
Nicolas Saporiti <nicolas.saporiti@gamil.com>
2022-01-26 21:26:15 -0300

    add currency getBtcPrice

--- src/helpers/getBtcPrice.js
+++ src/helpers/getBtcPrice.js
@@ -1,8 +1,11 @@
-export const getBtcPrice = async () => {
-  const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=clp';
+export const getBtcPrice = async (v) => {
+
+  const currency = v;
+
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}`;
   const res = await fetch(url);
   const data = await res.json();
-  const btcPrice = Number(data.bitcoin.clp);
+  const btcPrice = Number(data.bitcoin[currency]);
 
   return btcPrice;
 };
